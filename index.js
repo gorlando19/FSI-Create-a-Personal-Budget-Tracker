1544f3e5ca1a6d80f4c45adccf7876a29375ae0a
Gianfranco Orlando <gianfranco19@hotmail.com>
2022-01-15 17:19:51 -0500

    First release

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
+let answer = null
 
 // Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    console.log(answer)
+    weeklyExpenses += answer
+    console.log(weeklyExpenses)
+}
+answer = null
+for(let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    console.log(answer)
+    monthlyExpenses += answer
+    console.log(monthlyExpenses)
+}
+answer = null
+for(let i = 0; i < annualExpenseQuestions.length; i++) {
+    answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    console.log(answer)
+    annualExpenses += answer
+    console.log(annualExpenses)
+}
+
